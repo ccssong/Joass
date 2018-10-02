d18a7f2be066f83fc1e3ea1cea7b0faf3a52922f
ccssong <40797315+ccssong@users.noreply.github.com>
2018-10-02 16:49:28 +0900

    Update Network.java

--- ver1/sndToServer2/Network.java
+++ ver1/sndToServer2/Network.java
@@ -34,40 +34,11 @@ public class Network {
     int serverResponseCode = 0;
 
 
-    static final String FILE_PATH = Environment.getExternalStorageDirectory().getAbsolutePath();
+    static final String FILE_PATH = Environment.getExternalStorageDirectory().getPath();
     private static String TAG = "joassprac";
 
 
 
-    /*public void setHttpURLConnection(){
-        try{
-            //서버와 url connection
-            // FileInputStream fileInputStream = new FileInputStream(sourceFile);
-            URL url = new URL(uploadServerUrl);
-
-            //open a HTTP connection to the URL
-            conn = (HttpURLConnection) url.openConnection();
-            conn.setDoInput(true); //Allow inputs
-            conn.setDoOutput(true); // Allow outputs
-            conn.setUseCaches(false); //Don't use a Caches Copy
-
-            conn.setRequestMethod("POST");
-            //conn.setRequestProperty("ENCTYPE", "multipart/form-data");
-            conn.setRequestProperty("Connection", "Keep-Alive");
-            conn.setRequestProperty("Content-Type", "multipart/form-data;boundary=" + boundary);
-            conn.setRequestProperty("Cache-Control", "no-cache");
-            conn.setRequestProperty("uploaded_file", fileName);
-            conn.setReadTimeout(100*100*100);
-            conn.setConnectTimeout(100*100*100);
-
-            System.out.println("연결됐어~");
-        }
-        catch (IOException e)
-        {
-            System.out.println("연결 안됐어 멍청아!");
-        }
-
-    }*/
 
     public String upload(String param){
         int bytesRead, bytesAvailable, bufferSize;
@@ -99,20 +70,20 @@ public class Network {
                 dos.writeBytes("Content-Disposition: form-data; name=\"" + attachmentName + "\";filename=\"" + fileName + "\"" + lineEnd);
                 dos.writeBytes(lineEnd);
 
-                //create a buffer of maximum sisze
+                //create a buffer of maximum size
                 bytesAvailable = wavfile.available();
 
                 bufferSize = Math.min(bytesAvailable, maxBufferSize);
                 buffer = new byte[bufferSize];
 
                 //read file and write it into form...
-                bytesRead = sourceFile.read(buffer, 0, bufferSize);
+                bytesRead = wavfile.read(buffer, 0, bufferSize);
 
                 while (bytesRead > 0) {
                     dos.write(buffer, 0, bufferSize);
-                    bytesAvailable = sourceFile.available();
+                    bytesAvailable = wavfile.available();
                     bufferSize = Math.min(bytesAvailable, maxBufferSize);
-                    bytesRead = sourceFile.read(buffer, 0, bufferSize);
+                    bytesRead = wavfile.read(buffer, 0, bufferSize);
                 }
 
                 //send multipart from data necessary after file data..
@@ -150,13 +121,6 @@ public class Network {
             return(message);
         } catch (IOException e) {
             e.printStackTrace();
-
-            /*mainActivity.runOnUiThread(new Runnable() {
-                public void run() {
-                    responseText.setText("Got Exception : see logcat ");
-                    Toast.makeText(mainActivity, "Got Exception : see logcat ", Toast.LENGTH_SHORT).show();
-                }
-            });*/
             message = "Got Exception : see logcat " + e.getMessage();
             Log.e("Upload file to server", message, e);
             return(message);
